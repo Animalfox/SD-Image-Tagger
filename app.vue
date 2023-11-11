5dd7234363a4b7e2008362d1d909ca761a0b9a3c
Animalfox <a72e@outlook.com>
2023-11-11 21:42:18 +0300

    FEAT: Add NuxtImg and show images from folder

--- app.vue
+++ app.vue
@@ -2,9 +2,25 @@
 import { useStore } from '~/stores/default'
 
 const store = useStore()
+const previewSizes = 64;
 </script>
 
 <template>
-  <p>Folder <b>./images</b> contain:</p>
-  <pre>{{ store.images }}</pre>
+  <template v-if="store.images.length > 0">
+    <p>Your <b>./assets/images</b> folder contains:</p>
+    <div>
+      <NuxtImg v-for="image in store.images" :src="image" :width="previewSizes" :height="previewSizes" />
+    </div>
+  </template>
+  <template v-else>
+    <p>Your <b>./assets/images</b> folder is empty. Please add your images into this folder.</p>
+  </template>
 </template>
+
+<style>
+div {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+}
+</style>
--- stores/default.ts
+++ stores/default.ts
@@ -9,10 +9,10 @@ function getImages(query: string): Array<string> {
     interface globItem {
         default: string
     }
-    const glob = import.meta.glob('@/images/*.png', { eager: true }) as unknown as Array<globItem>
+    const glob = import.meta.glob('@/public/images/*.png', { eager: true }) as unknown as Array<globItem>
     let images: Array<string> = []
     Object.values(glob).forEach((g: globItem) => {
-        images.push(g.default)
+        images.push(g.default.replace('/_nuxt/public/',''))
     });
     return images
 }
