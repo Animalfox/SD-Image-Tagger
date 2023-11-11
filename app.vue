15aec109c9ed56c8817349d71ec91ae54ff6c88f
Animalfox <a72e@outlook.com>
2023-11-11 22:28:44 +0300

    STYLE: Move Image Gallery into a separate component

--- app.vue
+++ app.vue
@@ -1,37 +1,5 @@
-<script setup lang="ts">
-import { useStore } from "~/stores/default";
-
-const store = useStore();
-const previewSizes = 64;
-</script>
-
 <template>
   <main>
-    <template v-if="store.images.length > 0">
-      <p>Your <b>./assets/images</b> folder contains:</p>
-      <div>
-        <NuxtImg
-          v-for="image in store.images"
-          :key="image.path"
-          :src="image.path"
-          :width="previewSizes"
-          :height="previewSizes"
-        />
-      </div>
-    </template>
-    <template v-if="store.images.length == 0">
-      <p>
-        Your <b>./assets/images</b> folder is empty. Please add your images into
-        this folder.
-      </p>
-    </template>
+    <ImageGallery />
   </main>
 </template>
-
-<style lang="scss">
-div {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 10px;
-}
-</style>
---
+++ components/ImageGallery.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { useStore } from "@/stores/default";
+
+const store = useStore();
+const previewSizes = 64;
+</script>
+
+<template>
+  <section>
+    <h2>Image Gallery</h2>
+    <template v-if="store.images.length > 0">
+      <p>Your <b>./assets/images</b> folder contains:</p>
+      <div>
+        <NuxtImg
+          v-for="image in store.images"
+          :key="image.path"
+          :src="image.path"
+          :width="previewSizes"
+          :height="previewSizes"
+        />
+      </div>
+    </template>
+    <template v-if="store.images.length == 0">
+      <p>
+        Your <b>./assets/images</b> folder is empty. Please add your images into
+        this folder.
+      </p>
+    </template>
+  </section>
+</template>
+
+<style scoped lang="scss">
+div {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+}
+</style>
