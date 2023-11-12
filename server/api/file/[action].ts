117ba433eb89efee98ae469892040be9e94e3903
Animalfox <a72e@outlook.com>
2023-11-12 15:23:57 +0300

    FEAT: Integrate image file extension validator

--- server/api/file/[action].ts
+++ server/api/file/[action].ts
@@ -9,6 +9,12 @@ export default defineEventHandler((event) => {
   // Setup images folder for scanning files
   const imageFolder = process.cwd() + "\\assets\\images";
 
+  // Define valid image extensions
+  // By default SD Works with png only image types, but if you want to improve
+  // image filetypes compatibility you need to add new image extension to this
+  // array and convert image locally to png
+  const validImageExtensions = [".png"];
+
   // Send actions to functions
   switch (action) {
     case "list": {
@@ -22,38 +28,50 @@ export default defineEventHandler((event) => {
   }
 
   /**
-   * Scans image folder and return list of files
+   * Scans image folder, then prepare files and return list
    * @returns all file names array (includes non-image format)
    */
   function getAllFiles() {
     const files: Array<string> = [];
     fs.readdirSync(imageFolder).forEach((originalFullName) => {
-      // Describe original file
-      const original = {
-        name: path.parse(originalFullName).name,
-        ext: path.parse(originalFullName).ext,
-        uuidValid: validate(path.parse(originalFullName).name),
-      };
-      if (original.uuidValid) {
-        // It's ok, send file
-        files.push(originalFullName);
-      } else {
-        // Not ok, correct file name then send corrected
-        const newUuidFullName = uuidv4() + original.ext;
-        fs.rename(
-          imageFolder + "\\" + originalFullName,
-          imageFolder + "\\" + newUuidFullName,
-          (error) => {
-            if (error) {
-              console.log(error);
-            } else {
-              // File renamed
-              files.push(newUuidFullName);
-            }
-          },
-        );
+      // Process only valid (image) extension files
+      if (hasValidExtension(originalFullName, validImageExtensions)) {
+        // Describe original file
+        const original = {
+          name: path.parse(originalFullName).name,
+          ext: path.parse(originalFullName).ext,
+          uuidValid: validate(path.parse(originalFullName).name),
+        };
+        if (original.uuidValid) {
+          // It's ok, send file
+          files.push(originalFullName);
+        } else {
+          // Not ok, correct file name then send corrected
+          const newUuidFullName = uuidv4() + original.ext;
+          fs.rename(
+            imageFolder + "\\" + originalFullName,
+            imageFolder + "\\" + newUuidFullName,
+            (error) => {
+              if (!error) {
+                files.push(newUuidFullName);
+              }
+            },
+          );
+        }
       }
     });
     return files;
+  }
+
+  /**
+   * Function for validating file extension
+   * @param filename for validate
+   * @param exts array of allowed extensions
+   * @returns state of valid ( true | false )
+   */
+  function hasValidExtension(filename, exts) {
+    return new RegExp("(" + exts.join("|").replace(/\./g, "\\.") + ")$").test(
+      filename,
+    );
   }
 });
