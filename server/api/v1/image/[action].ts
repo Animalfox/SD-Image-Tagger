72a9516e471ecc6d34d399a6abf3d81090b97115
Animalfox <a72e@outlook.com>
2023-11-13 23:56:44 +0300

    FEAT: Automatically find and add tags into database from image folder

--- server/api/v1/image/[action].ts
+++ server/api/v1/image/[action].ts
@@ -3,6 +3,9 @@ import path from "node:path";
 import { v4 as uuidv4, validate } from "uuid";
 import { IImage } from "~/types";
 
+// Путь к файлу с тегами
+const tagsFilePath = process.cwd() + "\\database\\tags.json";
+
 export default defineEventHandler((event) => {
   // Detect file action from event
   const action = getRouterParam(event, "action");
@@ -30,6 +33,9 @@ export default defineEventHandler((event) => {
    * @returns all file names array (includes non-image format)
    */
   function getAllImages(): IImage[] {
+    // Загружаем теги из ./database/tags.json
+    const existingTags: string[] = loadTags();
+
     const images: IImage[] = [];
     fs.readdirSync(imageFolder).forEach((originalFullName) => {
       // Process only valid (image) extension files
@@ -43,11 +49,17 @@ export default defineEventHandler((event) => {
         if (original.uuidValid) {
           // It's ok, send file
           const tags = getTags(original.name);
-          const image: IImage = {
+          images.push({
             src: "/images/" + originalFullName,
             tags,
-          };
-          images.push(image);
+          });
+
+          // Добавляем теги изображения в массив existingTags
+          tags.forEach((tag) => {
+            if (!existingTags.includes(tag)) {
+              existingTags.push(tag);
+            }
+          });
         } else {
           // Not ok, correct file name then send corrected
           const newUuidFullName = uuidv4() + original.ext;
@@ -56,17 +68,21 @@ export default defineEventHandler((event) => {
             imageFolder + "\\" + newUuidFullName,
             (error) => {
               if (!error) {
-                const image: IImage = {
+                images.push({
                   src: "/images/" + newUuidFullName,
                   tags: [],
-                };
-                images.push(image);
+                });
               }
             },
           );
         }
       }
     });
+
+    // Сортируем и сохраняем обновленные теги в ./database/tags.json
+    existingTags.sort();
+    saveTags(existingTags);
+
     return images;
   }
 
@@ -92,11 +108,39 @@ export default defineEventHandler((event) => {
     if (fs.existsSync(txtFilePath)) {
       // If txt file exists, read and parse tags
       const tagsContent = fs.readFileSync(txtFilePath, "utf-8");
-      return tagsContent.split(",").map((tag) => tag.trim()).filter(Boolean);
+      return tagsContent
+        .split(",")
+        .map((tag) => tag.trim())
+        .filter(Boolean);
     } else {
       // If txt file doesn't exist, create it with an empty array of tags
       fs.writeFileSync(txtFilePath, "");
       return [];
+    }
+  }
+
+  /**
+   * Load tags from the tags file
+   * @returns array of tags
+   */
+  function loadTags(): string[] {
+    try {
+      const tagsContent = fs.readFileSync(tagsFilePath, "utf-8");
+      return JSON.parse(tagsContent);
+    } catch (error) {
+      return [];
+    }
+  }
+
+  /**
+   * Save tags to the tags file
+   * @param tags array of tags
+   */
+  function saveTags(tags: string[]): void {
+    try {
+      fs.writeFileSync(tagsFilePath, JSON.stringify(tags, null, 2), "utf-8");
+    } catch (error) {
+      // console.error("Error saving tags:", error.message);
     }
   }
 });
