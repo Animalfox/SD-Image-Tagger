8d5f60947f15bd7cad1536d9056a2d3d75649f6f
Animalfox <a72e@outlook.com>
2024-07-04 09:56:55 +0300

    Commit the final update before reorganize architecture

--- server/api/v1/image/[action].ts
+++ server/api/v1/image/[action].ts
@@ -1,146 +1,142 @@
-import fs from "node:fs";
-import path from "node:path";
-import { v4 as uuidv4, validate } from "uuid";
-import { useValidExtension } from "~/composables/useValidExtension";
-import { IImage } from "~/types";
+import fs from 'node:fs';
+import path from 'node:path';
+import { v4 as uuidv4, validate } from 'uuid';
+import { useValidExtension } from '~/composables/useValidExtension';
+import { IImage } from '~/types';
 
 // Path to a file with tags
-const tagsFilePath = process.cwd() + "\\database\\tags.json";
+const tagsFilePath = process.cwd() + '/database/tags.json';
 
 export default defineEventHandler((event) => {
-  // Detect file action from event
-  const action = getRouterParam(event, "action");
+	// Detect file action from event
+	const action = getRouterParam(event, 'action');
 
-  // Setup images folder for scanning files
-  const imageFolder = process.cwd() + "\\public\\images";
+	// Setup images folder for scanning files
+	const imageFolder = process.cwd() + '/public/images';
 
-  // Define valid image extensions
-  const validImageExtensions = [".png"];
+	// Define valid image extensions
+	const validImageExtensions = ['.png'];
 
-  // Send actions to functions
-  switch (action) {
-    case "list": {
-      return getAllImages();
-    }
-    case "tags": {
-      return getAllTags();
-    }
-    default: {
-      return {
-        error: `${action} is not implemented yet`,
-      };
-    }
-  }
+	// Send actions to functions
+	switch (action) {
+		case 'list': {
+			return getAllImages();
+		}
+		case 'tags': {
+			return getAllTags();
+		}
+		default: {
+			return {
+				error: `${action} is not implemented yet`,
+			};
+		}
+	}
 
-  /**
-   * Scans image folder, then prepare files and return list
-   * @returns all file names array (includes non-image format)
-   */
-  function getAllImages(): IImage[] {
-    // Загружаем теги из ./database/tags.json
-    const existingTags: string[] = loadTags();
+	/**
+	 * Scans image folder, then prepare files and return list
+	 * @returns all file names array (includes non-image format)
+	 */
+	function getAllImages(): IImage[] {
+		// Загружаем теги из ./database/tags.json
+		const existingTags: string[] = loadTags();
 
-    const images: IImage[] = [];
-    fs.readdirSync(imageFolder).forEach((originalFullName) => {
-      // Process only valid (image) extension files
-      if (useValidExtension(originalFullName, validImageExtensions)) {
-        // Describe original file
-        const original = {
-          name: path.parse(originalFullName).name,
-          ext: path.parse(originalFullName).ext,
-          uuidValid: validate(path.parse(originalFullName).name),
-        };
-        if (original.uuidValid) {
-          // It's ok, send file
-          const tags = getTags(original.name);
-          images.push({
-            src: "/images/" + originalFullName,
-            tags,
-          });
+		const images: IImage[] = [];
+		fs.readdirSync(imageFolder).forEach((originalFullName) => {
+			// Process only valid (image) extension files
+			if (useValidExtension(originalFullName, validImageExtensions)) {
+				// Describe original file
+				const original = {
+					name: path.parse(originalFullName).name,
+					ext: path.parse(originalFullName).ext,
+					uuidValid: validate(path.parse(originalFullName).name),
+				};
+				if (original.uuidValid) {
+					// It's ok, send file
+					const tags = getTags(original.name);
+					images.push({
+						src: '/images/' + originalFullName,
+						tags,
+					});
 
-          // Add image tags to the ExistingTags array
-          tags.forEach((tag) => {
-            if (!existingTags.includes(tag)) {
-              existingTags.push(tag);
-            }
-          });
-        } else {
-          // Not ok, correct file name then send corrected
-          const newUuidFullName = uuidv4() + original.ext;
-          fs.rename(
-            imageFolder + "\\" + originalFullName,
-            imageFolder + "\\" + newUuidFullName,
-            (error) => {
-              if (!error) {
-                images.push({
-                  src: "/images/" + newUuidFullName,
-                  tags: [],
-                });
-              }
-            },
-          );
-        }
-      }
-    });
+					// Add image tags to the ExistingTags array
+					tags.forEach((tag) => {
+						if (!existingTags.includes(tag)) {
+							existingTags.push(tag);
+						}
+					});
+				} else {
+					// Not ok, correct file name then send corrected
+					const newUuidFullName = uuidv4() + original.ext;
+					fs.rename(imageFolder + '/' + originalFullName, imageFolder + '/' + newUuidFullName, (error: any) => {
+						if (!error) {
+							images.push({
+								src: '/images/' + newUuidFullName,
+								tags: [],
+							});
+						}
+					});
+				}
+			}
+		});
 
-    // Sort and save updated tags in ./database/tags.json
-    existingTags.sort();
-    saveTags(existingTags);
+		// Sort and save updated tags in ./database/tags.json
+		existingTags.sort();
+		saveTags(existingTags);
 
-    return images;
-  }
+		return images;
+	}
 
-  /**
-   * Retrieve tags from txt file if it exists
-   * @param filename base filename without extension
-   * @returns array of tags or empty array if file doesn't exist or is empty
-   */
-  function getTags(filename: string): string[] {
-    const txtFilePath = path.join(imageFolder, filename + ".txt");
-    if (fs.existsSync(txtFilePath)) {
-      // If txt file exists, read and parse tags
-      const tagsContent = fs.readFileSync(txtFilePath, "utf-8");
-      return tagsContent
-        .split(",")
-        .map((tag) => tag.trim())
-        .filter(Boolean);
-    } else {
-      // If txt file doesn't exist, create it with an empty array of tags
-      fs.writeFileSync(txtFilePath, "");
-      return [];
-    }
-  }
+	/**
+	 * Retrieve tags from txt file if it exists
+	 * @param filename base filename without extension
+	 * @returns array of tags or empty array if file doesn't exist or is empty
+	 */
+	function getTags(filename: string): string[] {
+		const txtFilePath = path.join(imageFolder, filename + '.txt');
+		if (fs.existsSync(txtFilePath)) {
+			// If txt file exists, read and parse tags
+			const tagsContent = fs.readFileSync(txtFilePath, 'utf-8');
+			return tagsContent
+				.split(',')
+				.map((tag) => tag.trim())
+				.filter(Boolean);
+		} else {
+			// If txt file doesn't exist, create it with an empty array of tags
+			fs.writeFileSync(txtFilePath, '');
+			return [];
+		}
+	}
 
-  /**
-   * Load tags from the tags file
-   * @returns array of tags
-   */
-  function loadTags(): string[] {
-    try {
-      const tagsContent = fs.readFileSync(tagsFilePath, "utf-8");
-      return JSON.parse(tagsContent);
-    } catch (error) {
-      return [];
-    }
-  }
+	/**
+	 * Load tags from the tags file
+	 * @returns array of tags
+	 */
+	function loadTags(): string[] {
+		try {
+			const tagsContent = fs.readFileSync(tagsFilePath, 'utf-8');
+			return JSON.parse(tagsContent);
+		} catch (error) {
+			return [];
+		}
+	}
 
-  /**
-   * Save tags to the tags file
-   * @param tags array of tags
-   */
-  function saveTags(tags: string[]): void {
-    try {
-      fs.writeFileSync(tagsFilePath, JSON.stringify(tags, null, 2), "utf-8");
-    } catch (error) {
-      // console.error("Error saving tags:", error.message);
-    }
-  }
+	/**
+	 * Save tags to the tags file
+	 * @param tags array of tags
+	 */
+	function saveTags(tags: string[]): void {
+		try {
+			fs.writeFileSync(tagsFilePath, JSON.stringify(tags, null, 2), 'utf-8');
+		} catch (error) {
+			// console.error("Error saving tags:", error.message);
+		}
+	}
 
-  /**
-   * Get all tags from the tags file
-   * @returns array of tags
-   */
-  function getAllTags(): string[] {
-    return loadTags();
-  }
+	/**
+	 * Get all tags from the tags file
+	 * @returns array of tags
+	 */
+	function getAllTags(): string[] {
+		return loadTags();
+	}
 });
