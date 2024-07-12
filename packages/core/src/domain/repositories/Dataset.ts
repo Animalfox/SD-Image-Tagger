35bcba7f125ee06ffc0f561e45ff86caa2394a18
Animalfox <a72e@outlook.com>
2024-07-13 02:30:25 +0300

    chore: prettify

--- packages/core/src/domain/repositories/Dataset.ts
+++ packages/core/src/domain/repositories/Dataset.ts
@@ -2,9 +2,9 @@ import Image from "../entities/Image";
 import { Path } from "../entities/Path";
 
 export default interface Dataset {
-    constructor(path: Path): boolean;
-    GetImageById(): Promise<Array<Image>>;
-    CreateImage(): Promise<boolean>;
-    UpdateImage(): Promise<boolean>;
-    DeleteImage(): Promise<boolean>;
+  constructor(path: Path): boolean;
+  GetImageById(): Promise<Array<Image>>;
+  CreateImage(): Promise<boolean>;
+  UpdateImage(): Promise<boolean>;
+  DeleteImage(): Promise<boolean>;
 }
--- packages/core/src/infrastructure/repositories/FolderDataset.ts
+++ packages/core/src/infrastructure/repositories/FolderDataset.ts
@@ -4,37 +4,35 @@ import Dataset from "../../domain/repositories/Dataset";
 import fs from "node:fs";
 
 export const useValidExtension = (filename: string, exts: string[]) => {
-    return new RegExp("(" + exts.join("|").replace(/\./g, "\\.") + ")$").test(
-      filename,
-    );
-  };
+  return new RegExp("(" + exts.join("|").replace(/\./g, "\\.") + ")$").test(filename);
+};
 
 export default class FolderDataset implements Dataset {
-    private path: Path;
-    ["constructor"](path: Path): boolean {
-        this.path = path;
-        return this.isPathCorrect(path);
-    }
-    async GetImageById(): Promise<Array<Image>> {
-        const images: Array<Image> = [];
-        const isPng = (path: Path) => {
-            return new RegExp("(" + "png".replace(/\./g, "\\.") + ")$").test(path);
-        }
-        const pngArr = await fs.readdirSync(this.path).filter(elem => isPng(elem));
-        /** Because image interface is empty, nothing to add into images: empty elements */
-        pngArr.forEach(i => images.push(new Image()));
-        return images;
-    }
-    CreateImage(): Promise<boolean> {
-        throw new Error("Method not implemented.");
-    }
-    UpdateImage(): Promise<boolean> {
-        throw new Error("Method not implemented.");
-    }
-    DeleteImage(): Promise<boolean> {
-        throw new Error("Method not implemented.");
-    }
-    private isPathCorrect(path: Path): boolean {
-        return fs.lstatSync(path).isDirectory();
-    }
+  private path: Path;
+  ["constructor"](path: Path): boolean {
+    this.path = path;
+    return this.isPathCorrect(path);
+  }
+  async GetImageById(): Promise<Array<Image>> {
+    const images: Array<Image> = [];
+    const isPng = (path: Path) => {
+      return new RegExp("(" + "png".replace(/\./g, "\\.") + ")$").test(path);
+    };
+    const pngArr = await fs.readdirSync(this.path).filter(elem => isPng(elem));
+    /** Because image interface is empty, nothing to add into images: empty elements */
+    pngArr.forEach(i => images.push(new Image()));
+    return images;
+  }
+  CreateImage(): Promise<boolean> {
+    throw new Error("Method not implemented.");
+  }
+  UpdateImage(): Promise<boolean> {
+    throw new Error("Method not implemented.");
+  }
+  DeleteImage(): Promise<boolean> {
+    throw new Error("Method not implemented.");
+  }
+  private isPathCorrect(path: Path): boolean {
+    return fs.lstatSync(path).isDirectory();
+  }
 }
