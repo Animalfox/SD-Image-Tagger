5acacf06b46e85851f437b09a874ce6880948c6e
Animalfox <a72e@outlook.com>
2024-07-13 02:29:17 +0300

    Implements FolderDataset.GetImageById function

--- packages/core/src/infrastructure/repositories/FolderDataset.ts
+++ packages/core/src/infrastructure/repositories/FolderDataset.ts
@@ -3,14 +3,27 @@ import { Path } from "../../domain/entities/Path";
 import Dataset from "../../domain/repositories/Dataset";
 import fs from "node:fs";
 
+export const useValidExtension = (filename: string, exts: string[]) => {
+    return new RegExp("(" + exts.join("|").replace(/\./g, "\\.") + ")$").test(
+      filename,
+    );
+  };
+
 export default class FolderDataset implements Dataset {
     private path: Path;
     ["constructor"](path: Path): boolean {
         this.path = path;
         return this.isPathCorrect(path);
     }
-    GetImageById(): Promise<Array<Image>> {
-        throw new Error("Method not implemented.");
+    async GetImageById(): Promise<Array<Image>> {
+        const images: Array<Image> = [];
+        const isPng = (path: Path) => {
+            return new RegExp("(" + "png".replace(/\./g, "\\.") + ")$").test(path);
+        }
+        const pngArr = await fs.readdirSync(this.path).filter(elem => isPng(elem));
+        /** Because image interface is empty, nothing to add into images: empty elements */
+        pngArr.forEach(i => images.push(new Image()));
+        return images;
     }
     CreateImage(): Promise<boolean> {
         throw new Error("Method not implemented.");
