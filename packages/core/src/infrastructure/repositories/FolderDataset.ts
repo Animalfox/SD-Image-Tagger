5bb6a3dc2427f5469cbe0ad16aaf6da64bff8bbd
Animalfox <a72e@outlook.com>
2024-07-13 01:40:17 +0300

    Update FolderDataset repository

--- packages/core/src/infrastructure/repositories/FolderDataset.ts
+++ packages/core/src/infrastructure/repositories/FolderDataset.ts
@@ -1,3 +1,17 @@
-import Dataset from "../../domain/interfaces/Dataset";
+import Image from "../../domain/entities/Image";
+import Dataset from "../../domain/repositories/Dataset";
 
-export default class FolderDataset implements Dataset {}
+export default class FolderDataset implements Dataset {
+    GetImageById(): Promise<Array<Image>> {
+        throw new Error("Method not implemented.");
+    }
+    CreateImage(): Promise<boolean> {
+        throw new Error("Method not implemented.");
+    }
+    UpdateImage(): Promise<boolean> {
+        throw new Error("Method not implemented.");
+    }
+    DeleteImage(): Promise<boolean> {
+        throw new Error("Method not implemented.");
+    }
+}
