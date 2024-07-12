702848d344fa3cc211b336a1ac9939aad9465cd7
Animalfox <a72e@outlook.com>
2024-07-12 22:41:54 +0300

    Move current outdated solution into packages/nuxt folder

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+/** @format */
+
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParents: "avoid",
+  rangeStart: 0,
+  rangeEnd: Infinity,
+  proseWrap: "preserve",
+  requirePragma: false,
+  insertPragma: false,
+}
