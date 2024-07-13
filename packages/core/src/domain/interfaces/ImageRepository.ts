b7dcd92ac4baab26eeb3ff5fe938a8ef608495da
Animalfox <a72e@outlook.com>
2024-07-13 03:30:01 +0300

    Describe ImageRepository Interface

--- packages/core/src/domain/interfaces/ImageRepository.ts
+++ packages/core/src/domain/interfaces/ImageRepository.ts
@@ -1,8 +1,36 @@
 import Image from "../entities/Image";
 
+/**
+ * Interface for the Image repository, providing basic CRUD operations.
+ * 
+ * This interface defines methods for creating, retrieving, updating, and deleting images.
+ */
 export default interface ImageRepository {
-  GetImageById(): Promise<Array<Image>>;
-  CreateImage(): Promise<boolean>;
-  UpdateImage(): Promise<boolean>;
-  DeleteImage(): Promise<boolean>;
+    /**
+     * Retrieves an image by its ID.
+     * 
+     * @returns {Promise<Array<Image>>} - A promise that resolves to an array of `Image` objects.
+     */
+    GetImageById(): Promise<Array<Image>>;
+
+    /**
+     * Creates a new image.
+     * 
+     * @returns {Promise<boolean>} - A promise that resolves to a boolean indicating the success of the operation.
+     */
+    CreateImage(): Promise<boolean>;
+
+    /**
+     * Updates an existing image.
+     * 
+     * @returns {Promise<boolean>} - A promise that resolves to a boolean indicating the success of the operation.
+     */
+    UpdateImage(): Promise<boolean>;
+
+    /**
+     * Deletes an image.
+     * 
+     * @returns {Promise<boolean>} - A promise that resolves to a boolean indicating the success of the operation.
+     */
+    DeleteImage(): Promise<boolean>;
 }
