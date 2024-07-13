da904a366cf72adf2cdcf0e78d023872a4336d92
Animalfox <a72e@outlook.com>
2024-07-13 03:30:37 +0300

    style: prettify

--- packages/core/src/domain/interfaces/ImageRepository.ts
+++ packages/core/src/domain/interfaces/ImageRepository.ts
@@ -2,35 +2,35 @@ import Image from "../entities/Image";
 
 /**
  * Interface for the Image repository, providing basic CRUD operations.
- * 
+ *
  * This interface defines methods for creating, retrieving, updating, and deleting images.
  */
 export default interface ImageRepository {
-    /**
-     * Retrieves an image by its ID.
-     * 
-     * @returns {Promise<Array<Image>>} - A promise that resolves to an array of `Image` objects.
-     */
-    GetImageById(): Promise<Array<Image>>;
+  /**
+   * Retrieves an image by its ID.
+   *
+   * @returns {Promise<Array<Image>>} - A promise that resolves to an array of `Image` objects.
+   */
+  GetImageById(): Promise<Array<Image>>;
 
-    /**
-     * Creates a new image.
-     * 
-     * @returns {Promise<boolean>} - A promise that resolves to a boolean indicating the success of the operation.
-     */
-    CreateImage(): Promise<boolean>;
+  /**
+   * Creates a new image.
+   *
+   * @returns {Promise<boolean>} - A promise that resolves to a boolean indicating the success of the operation.
+   */
+  CreateImage(): Promise<boolean>;
 
-    /**
-     * Updates an existing image.
-     * 
-     * @returns {Promise<boolean>} - A promise that resolves to a boolean indicating the success of the operation.
-     */
-    UpdateImage(): Promise<boolean>;
+  /**
+   * Updates an existing image.
+   *
+   * @returns {Promise<boolean>} - A promise that resolves to a boolean indicating the success of the operation.
+   */
+  UpdateImage(): Promise<boolean>;
 
-    /**
-     * Deletes an image.
-     * 
-     * @returns {Promise<boolean>} - A promise that resolves to a boolean indicating the success of the operation.
-     */
-    DeleteImage(): Promise<boolean>;
+  /**
+   * Deletes an image.
+   *
+   * @returns {Promise<boolean>} - A promise that resolves to a boolean indicating the success of the operation.
+   */
+  DeleteImage(): Promise<boolean>;
 }
