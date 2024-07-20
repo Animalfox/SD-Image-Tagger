f3fc7f555cc53227952790ac13b0518a206ae2f6
Animalfox <a72e@outlook.com>
2024-07-20 16:37:02 +0300

    Update Read methods of TagRepository interface and refresh it docs

--- packages/core/src/domain/interfaces/TagRepository.ts
+++ packages/core/src/domain/interfaces/TagRepository.ts
@@ -10,22 +10,33 @@ export default interface TagRepository {
   /**
    * Creates a new tag.
    *
+   * @param {string} name - The name of the tag.
    * @returns {Promise<boolean>} - A promise that resolves to a boolean
    * indicating the success of the operation.
    */
   createTag(name: string): Promise<boolean>;
 
   /**
-   * Reads a tag by its ID.
+   * Reads all tags.
    *
    * @returns {Promise<Array<Tag>>} - A promise that resolves to an array of
    * `Tag` objects.
    */
-  getTag(name: string): Promise<Tag | null>;
+  getAllTags(): Promise<Array<Tag>>;
+
+  /**
+   * Reads a tag by its name.
+   *
+   * @param {string} name - The name of the tag.
+   * @returns {Promise<Tag | null>} - A promise that resolves to a `Tag` object
+   * or null if not found.
+   */
+  getTagByName(name: string): Promise<Tag | null>;
 
   /**
    * Updates an existing tag.
    *
+   * @param {string} name - The name of the tag.
    * @returns {Promise<boolean>} - A promise that resolves to a boolean
    * indicating the success of the operation.
    */
@@ -34,6 +45,7 @@ export default interface TagRepository {
   /**
    * Deletes a tag.
    *
+   * @param {string} name - The name of the tag.
    * @returns {Promise<boolean>} - A promise that resolves to a boolean
    * indicating the success of the operation.
    */
