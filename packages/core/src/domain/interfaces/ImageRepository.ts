2788739fd6c152cedf58499a9002c04b1b60cdcd
Animalfox <a72e@outlook.com>
2024-07-20 12:39:10 +0300

    docs: typo

--- packages/core/src/domain/interfaces/ImageRepository.ts
+++ packages/core/src/domain/interfaces/ImageRepository.ts
@@ -3,34 +3,39 @@ import Image from "@domain/entities/Image";
 /**
  * Interface for the Image repository, providing basic CRUD operations.
  *
- * This interface defines methods for creating, retrieving, updating, and deleting images.
+ * This interface defines methods for creating, retrieving, updating, and
+ * deleting images.
  */
 export default interface ImageRepository {
   /**
    * Creates a new image.
    *
-   * @returns {Promise<boolean>} - A promise that resolves to a boolean indicating the success of the operation.
+   * @returns {Promise<boolean>} - A promise that resolves to a boolean
+   * indicating the success of the operation.
    */
-  CreateImage(): Promise<boolean>;
+  createImage(): Promise<boolean>;
 
   /**
-   * Read an image by its ID.
+   * Reads an image by its ID.
    *
-   * @returns {Promise<Array<Image>>} - A promise that resolves to an array of `Image` objects.
+   * @returns {Promise<Array<Image>>} - A promise that resolves to an array of
+   * `Image` objects.
    */
-  GetImageById(): Promise<Array<Image>>;
+  getImageById(): Promise<Array<Image>>;
 
   /**
    * Updates an existing image.
    *
-   * @returns {Promise<boolean>} - A promise that resolves to a boolean indicating the success of the operation.
+   * @returns {Promise<boolean>} - A promise that resolves to a boolean
+   * indicating the success of the operation.
    */
-  UpdateImage(): Promise<boolean>;
+  updateImage(): Promise<boolean>;
 
   /**
    * Deletes an image.
    *
-   * @returns {Promise<boolean>} - A promise that resolves to a boolean indicating the success of the operation.
+   * @returns {Promise<boolean>} - A promise that resolves to a boolean
+   * indicating the success of the operation.
    */
-  DeleteImage(): Promise<boolean>;
+  deleteImage(): Promise<boolean>;
 }
