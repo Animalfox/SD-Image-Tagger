7d8481d758e6b9f2d20d5a8e748b30ab8b4231e5
Animalfox <a72e@outlook.com>
2024-07-20 12:15:12 +0300

    docs: Typo

--- packages/core/src/domain/entities/Image.ts
+++ packages/core/src/domain/entities/Image.ts
@@ -1,9 +1,10 @@
 /**
  * Represents an image with a path.
  *
- * Objects of the `Image` class are used as child elements of the boundary `ImageRepository` interface.
- * They provide a summary of useful information about an image, including its path and tags.
- * Currently, tags are not included in the image as the class because they still under development.
+ * Objects of the `Image` class are used as child elements of the boundary
+ * `ImageRepository` interface. They provide a summary of useful information
+ * about an image, including its path and tags. Currently, tags are not included
+ * in the Image class as they are still under development.
  */
 export default class Image {
   private path: string;
@@ -14,6 +15,6 @@ export default class Image {
    * @param {string} path - The path to the image.
    */
   constructor(path: string) {
-    this.path = path;
+      this.path = path;
   }
 }
