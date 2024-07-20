f4e20c47407ccb5c4257f57b97e9e5c7fbb30c1b
Animalfox <a72e@outlook.com>
2024-07-20 17:36:11 +0300

    docs: shrink description by 80 ruler

--- packages/core/src/domain/entities/Image.ts
+++ packages/core/src/domain/entities/Image.ts
@@ -3,8 +3,8 @@ import Tag from "./Tag";
 /**
  * Represents an image with a path and tags.
  *
- * The `Image` class is used to manage the path and tags associated with an image.
- * It provides methods to create, read, update, and delete tags.
+ * The `Image` class is used to manage the path and tags associated with an
+ * image. It provides methods to create, read, update, and delete tags.
  */
 export default class Image {
   private _path: string;
